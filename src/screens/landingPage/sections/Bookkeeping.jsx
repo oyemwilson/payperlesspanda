d611d0de8ae32fdd9a665ade63d4765965f6d824
Wilson Oyem <85078140+oyemwilson@users.noreply.github.com>
2025-06-26 12:16:48 +0100

    Update Bookkeeping.jsx

--- src/screens/landingPage/sections/Bookkeeping.jsx
+++ src/screens/landingPage/sections/Bookkeeping.jsx
@@ -4,7 +4,7 @@ const Bookkeeping = () => {
     return (
         <div className='bg-black py-20'>
             <div className='bg-green-950 min-h-[50vh] flex flex-col items-center text-gray-100 justify-center text-center ' style={{ clipPath: 'polygon(100% 0, 100% 100%, 0% 90%, 0% 15%)' }}>
-                <h1 className='z-50  lg:text-6xl w-[75%]'data-aos="fade-up"
+                <h1 className='z-50 text-3xl w-[95%] lg:text-6xl md:w-[75%]'data-aos="fade-up"
      data-aos-duration="2000">You didn't start your business to be a bookkeeper Ditch the Ledger, Chase Your Dreams </h1>
 
             </div>
